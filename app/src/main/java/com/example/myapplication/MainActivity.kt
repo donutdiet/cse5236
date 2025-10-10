ec12c8fb60dcd9b208603344b9a638a06c120036
donutdiet <72674098+donutdiet@users.noreply.github.com>
2025-10-09 20:38:06 -0400

    Add profile tab to bottom nav

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,3 @@
-//package com.example.myapplication
-//
-//import android.os.Bundle
-//import androidx.activity.enableEdgeToEdge
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.view.ViewCompat
-//import androidx.core.view.WindowInsetsCompat
-//
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//    }
-//}
-
 package com.example.myapplication
 
 import android.os.Bundle
@@ -37,13 +16,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Load default fragment
-        replaceFragment(TrackFragment())
+        replaceFragment(MapFragment())
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.nav_track -> replaceFragment(TrackFragment())
+                R.id.nav_map -> replaceFragment(MapFragment())
                 R.id.nav_report -> replaceFragment(ReportFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
             }
             true
         }
--- app/src/main/java/com/example/myapplication/MapFragment.kt
+++ app/src/main/java/com/example/myapplication/MapFragment.kt
@@ -7,8 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class TrackFragment : Fragment() {
-    private val TAG = "TrackFragment"
+class MapFragment : Fragment() {
+    private val TAG = "MapFragment"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +20,7 @@ class TrackFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         Log.d(TAG, "onCreateView")
-        return inflater.inflate(R.layout.fragment_track, container, false)
+        return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
     override fun onStart() {
