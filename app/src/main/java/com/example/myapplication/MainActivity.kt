72cbd96a6a64a40a0f8164b5980bb0ebad6a50e1
donutdiet <72674098+donutdiet@users.noreply.github.com>
2025-10-31 13:05:38 -0400

    Update bottom nav to use navController

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,38 +2,26 @@ package com.example.myapplication
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.example.myapplication.map.MapFragment
-import com.example.myapplication.profile.ProfileFragment
-import com.example.myapplication.reports.AllReportsFragment
-import com.example.myapplication.reports.ReportFragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     val mainActTag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(mainActTag, "onCreate")
 
         setContentView(R.layout.activity_main)
-        currentFragment(MapFragment())
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
-        bottomNav.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_map -> currentFragment(MapFragment())
-                R.id.nav_report -> currentFragment(ReportFragment())
-                R.id.nav_allReports -> currentFragment(AllReportsFragment())
-                R.id.nav_profile -> currentFragment(ProfileFragment())
-            }
-            true
-        }
+        bottomNav.setupWithNavController(navController)
     }
-    private fun currentFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, fragment)
-            .commit()
-    }
+
     override fun onStart() {
         super.onStart()
         Log.d(mainActTag, "onStart")
